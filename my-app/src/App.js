ae07d99978a63555e35f535f42e91cb3fe97ac64
dhivyasa <dhivyasa@github.com>
2024-04-10 12:50:16 -0400

    initial boxes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function Story() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="square">
+        <Sentence/>
+        <Sentence/>
+      </div>
+    </>
   );
 }
+const Originalvalue="En Peru Arya"
 
-export default App;
+function Sentence() {
+  const [value, setValue] = useState(Originalvalue);
+  function handleClick() {
+    setValue("My name is Arya");
+    console.log("clicked!");
+  }
+  return (
+    <button onClick={handleClick} className="square">
+      {value}
+    </button>
+  );
+}
